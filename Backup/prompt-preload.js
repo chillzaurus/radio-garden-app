b8ba6e07a679a9534a25e1620f469a9b0f120e56
chillzaurus <mailsubcer@gmail.com>
2026-02-28 15:47:43 +0200

    clean up files

--- Backup/prompt-preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('promptAPI', {
-  submit: (value) => ipcRenderer.send('prompt-result', value),
-  cancel: () => ipcRenderer.send('prompt-result', null)
-});
